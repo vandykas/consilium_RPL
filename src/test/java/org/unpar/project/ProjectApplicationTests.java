c37171af1c480b64810ac879a5ed31b2a9508f92
Vandyka Suryadi <vandykasuryadi@gmail.com>
2025-11-18 23:51:12 +0700

    Initialize project

---
+++ src/test/java/org/unpar/project/ProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package org.unpar.project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
